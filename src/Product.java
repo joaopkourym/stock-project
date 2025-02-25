b75cb9a6c36e3a00ea93a06c2aed447d4d1b19d2
joaopkourym <jp.kourymendoca@gmail.com>
2025-02-25 17:58:30 -0300

    feat: Implementing a try-catch block in the product price input for error handling and printing current stock quantity when added or removed

--- src/Product.java
+++ src/Product.java
@@ -1,12 +1,13 @@
-    import java.util.Scanner;
+import java.util.Scanner;
 
 public class Product {
     private String productName;
-    private double price;
+    private Double price;
     private double totalPrice;
     private int stockAddedQuantity;
     private int stockRemovedQuantity;
     private int stockQuantity;
+    private int selection;
     Scanner scanner = new Scanner(System.in);
         
     public void showProductData() {
@@ -15,28 +16,34 @@ public class Product {
         productName = scanner.next();
 
         System.out.print("Price: ");
-        price = scanner.nextDouble();
-
-        System.out.print("Quantity in stock: ");
-        stockQuantity = scanner.nextInt();
-
-        totalPrice = price * stockQuantity;
-
-        System.out.format("Product data: %s, $%.2f, %d units, Total price: $%.2f\n", productName, price, stockQuantity, totalPrice);
+        
+        try {
+            price = scanner.nextDouble();
+            System.out.print("Quantity in stock: ");
+            stockQuantity = scanner.nextInt();
+    
+            totalPrice = price * stockQuantity;
+    
+            System.out.format("Product data: %s, $%.2f, %d units, Total price: $%.2f\n", productName, price, stockQuantity, totalPrice);
+            System.out.println("Select an option: (1)Remove product stock quantity | (2)Add product stock quantity");
+            selection = scanner.nextInt();
+        } catch (Exception erro) {
+            System.out.println("Error!!! Put a valid price");
+        }
     }
 
     public void addProductQuantity() {
-        System.out.print("Enter the number of products to be added in stock: ");
+        System.out.println("Enter the number of products to be added in stock: ");
         stockAddedQuantity = scanner.nextInt();
-        System.out.println(stockQuantity);
         stockQuantity += stockAddedQuantity;
-        System.out.println(stockQuantity);
-    
+        System.out.format("Current stock quantity: %d", stockQuantity);
     }
 
     public void removeProductQuantity() {
-        System.out.print("Enter the number of products to be removed from stock: ");
+        System.out.println("Enter the number of products to be removed from stock: ");
         stockRemovedQuantity = scanner.nextInt();
         stockQuantity -= stockRemovedQuantity;
+        System.out.format("Current stock quantity: %d", stockQuantity);
     }
 }
+
